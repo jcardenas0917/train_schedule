19ca6e99d10d1b9b8066a728d8169a13574fe731
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-07-16 21:23:35 -0400

    connected to firebase

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,13 +1,45 @@
+$(document).ready(function () {
+  console.log("ready!");
 
- // Set the configuration for your app
-  // TODO: Replace with your project's config object
-  var config = {
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+  const firebaseConfig = {
     apiKey: "AIzaSyCZuq4ogqQbBDn3XRjmOLL4eDN-zvoWvaA",
     authDomain: "train-schedule-ucf.firebaseapp.com",
-    databaseURL: "https://train-schedule-ucf.firebaseio.com/",
-    storageBucket: "gs://train-schedule-ucf.appspot.com/"
+    databaseURL: "https://train-schedule-ucf.firebaseio.com",
+    projectId: "train-schedule-ucf",
+    storageBucket: "train-schedule-ucf.appspot.com",
+    messagingSenderId: "174634230088",
+    appId: "1:174634230088:web:b8c47afbb6c93d07"
   };
-  firebase.initializeApp(config);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase);
 
-  // Get a reference to the database service
-  var database = firebase.database();
+// // Get a reference to the database service
+// var database = firebase.database();
+
+
+$("#submit").on("click", function (value) {
+event.preventDefault();
+//Set input variables from user
+var train = {
+  trainName: $("#tname").val(),
+  destination: $("#destination").val(),
+  firstTrain: $("#firstTrain").val(),
+  frequency: $("#frequency").val(),
+}
+  //console loging for debuging
+  console.log(train.trainName);
+  console.log(train.destination);
+  console.log(train.firstTrain);
+  console.log(train.frequency);
+});
+
+
+
+  
+ 
+
+
+});
