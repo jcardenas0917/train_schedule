a0166ccc9710c3e39d4cbe39501c9573d9d9da94
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-07-19 17:46:14 -0400

    trying to fix the time

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -13,53 +13,69 @@ const firebaseConfig = {
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  console.log(firebase);
- 
+
   // Get a reference to the database 
   var database = firebase.database().ref("train");
-  
- 
-  var currentTime = moment().format('h:mm A');
-  $("#currentTime").text(currentTime);
-  console.log(currentTime);
+  console.log(database);
+  database.on("child_added", function (snapshot) {
+    var train = snapshot.val()
+    console.log(train);
+    var tBody = $("#trainDisplay");
+    var tRow = $("<tr>");
+    var trainName = $("<th>").text(train.name);
+    var trainDest = $("<th>").text(train.destination);
+    var trainFreq = $("<th>").text(train.frequency + "mm");
+    var trainNext = $("<th>").text(train.next_train);
+    var minAway = $("<th>").text(train.min_away);
 
-function writeData(name,destination,time,frequency){
-    var data = database.push({ 
+    // Append the newly created table data to the table row
+    tRow.append(trainName, trainDest, trainFreq, trainNext, minAway);
+    // Append the table row to the table body
+    tBody.append(tRow);
+  });
+ 
+  var currentTimeDisplay = moment().format('hh:mm A');
+  $("#currentTime").text(currentTimeDisplay);
+  
+
+function writeData(name,destination,next,frequency,away){
+    database.push({ 
     name: name,
     destination: destination,
-    first_train: time,
+    next_train: next,
     frequency: frequency,
+    min_away: away
   });
-  data.on('value',displayData);
+  // data.on('value',displayData);
 }
 
-function displayData(snapshot){
+// function displayData(snapshot){
   
-  var train = snapshot.val();
+//   var train = snapshot.val();
 
-  //console log for debugging
-  console.log(train.destination);
-  console.log(train.first_train);
-  console.log(train.frequency);
-  console.log(train.name);
+//   //console log for debugging
+//   console.log(train.destination);
+//   console.log(train.first_train);
+//   console.log(train.frequency);
+//   console.log(train.name);
 
   
-  var tBody = $("#trainDisplay");
-  var tRow = $("<tr>");
-  var trainName = $("<th>").text(train.name);
-  var trainDest = $("<th>").text(train.destination);
-  var trainFreq = $("<th>").text(train.frequency + "mm");
-  var trainNext = $("<th>").text(train.first_train);
-  var minAway = $("<th>").text("TBD");
+//   var tBody = $("#trainDisplay");
+//   var tRow = $("<tr>");
+//   var trainName = $("<th>").text(train.name);
+//   var trainDest = $("<th>").text(train.destination);
+//   var trainFreq = $("<th>").text(train.frequency + "mm");
+//   var trainNext = $("<th>").text(train.first_train);
+//   var minAway = $("<th>").text("TBD");
 
-  // Append the newly created table data to the table row
-  tRow.append(trainName, trainDest, trainFreq,trainNext,minAway);
-  // Append the table row to the table body
-  tBody.append(tRow);
+//   // Append the newly created table data to the table row
+//   tRow.append(trainName, trainDest, trainFreq,trainNext,minAway);
+//   // Append the table row to the table body
+//   tBody.append(tRow);
   
-}
+// }
 
-$("#submit").on("click", function (value) {
+$("#submit").on("click", function (event) {
 event.preventDefault();
 //Set input variables from user
 
@@ -69,15 +85,44 @@ event.preventDefault();
   var frequency= $("#frequency").val();
 
 //train time calculations go here and result will sent to writeData function.
-  var militaryTime = moment(firstTrain,'HH:mm').format('hh:mm a'); 
 
-  var frequencyTime = moment(frequency,'mm').format(':mm');
 
-  console.log(frequencyTime);
+  var firstTime = moment(firstTrain, "hh:mm").subtract(1, "years");
+  console.log(firstTime);
 
-  writeData(trainName,destination,militaryTime,frequencyTime);
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTime), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % frequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var nextTrainMin = frequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + nextTrainMin);
+
+  // Next Train
+  var nextTrain = moment().add(nextTrainMin, "minutes").format("hh:mm A");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+  var frequencyTime = moment(frequency, 'mm').format(':mm');
+  
+  // var firstTrainTime = moment(firstTrain,'HH:mm').format('hh:mm a'); 
+  // console.log(firstTrainTime);
+  
+  
+  // // var nextTrainTime = moment(firstTrain).add(2,'hours');
+
+  // console.log(frequencyTime);
+
+  writeData(trainName, destination, nextTrain, frequencyTime,nextTrainMin );
+  
 })
-
-
+  
 
 });
